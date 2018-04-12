f1286cb7d98efac59d426529fe6d5c2f3f855dfb
cwang101 <625250454@qq.com>
2018-04-13 01:24:17 +0800

    The InverseTest complete!

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -16,6 +17,12 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        int des=random.nextInt(3);
+        List<Integer> result=new ArrayList<>();
+
+        while((number-=des)>=0){
+            result.add(number);
+        }
+       return result;
     }
 }
